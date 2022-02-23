6120a5a1b3e86d0f476be3028272546bdfe4d33a
shane panter <shane@foundationcode.com>
2022-02-23 15:19:06 -0700

    Updated starter code

--- app/src/main/java/lesson/App.java
+++ app/src/main/java/lesson/App.java
@@ -6,15 +6,10 @@ public class App {
         Scanner scnr = new Scanner(System.in);
   
         // TODO: Declare a LinkedList called shoppingList of type ListItem
-  
-        String item;
         
-        // TODO: Scan inputs (items) and add them to the shoppingList LinkedList
-        //       Read inputs until a -1 is input
+        // TODO: Scan inputs (items) using the static method on ListItem
        
-        
-        // TODO: Print the shoppingList LinkedList using the printNodeData() method
+        // TODO: Print the shoppingList 
       
-  
      }
 }
--- app/src/main/java/lesson/ListItem.java
+++ app/src/main/java/lesson/ListItem.java
@@ -1,18 +1,5 @@
 package lesson;
 
 public class ListItem {
-    private String item;
 
-    public ListItem() {
-        item = "";
-    }
-
-    public ListItem(String itemInit) {
-        this.item = itemInit;
-    }
-    
-    @Override
-    public String toString(){
-        return item;
-    }
 }
